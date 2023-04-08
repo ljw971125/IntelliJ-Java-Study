7f52d313d801d985687b3c5394a9707b0fac08cf
ljw971125 <ljw971125@naver.com>
2023-04-08 22:04:26 +0900

    자바 20230408 제어문
    자바 20230408 제어문(주석추가)

---
+++ Java_Study/Java_20230408_Study/src/Study_04.java
@@ -0,0 +1,9 @@
+public class Study_04 {
+    public static void main(String[] args) {
+        int a = 15;
+        if(a>10) { //a가 10보다 클경우
+            int b = a - 10; //b의 값은 a-10
+            System.out.println(b);
+        }
+    }
+}
