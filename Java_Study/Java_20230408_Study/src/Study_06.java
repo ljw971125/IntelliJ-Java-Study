ec7fe2e0f19eb43a6962d13f2d68d5950e0d773b
ljw971125 <ljw971125@naver.com>
2023-04-09 00:40:49 +0900

    자바 20230408 정수형 long

---
+++ Java_Study/Java_20230408_Study/src/Study_06.java
@@ -0,0 +1,16 @@
+public class Study_06 {
+    public static void main(String[] args) {
+      long var1 = 10;
+      long var2 = 20L;
+      //long var3 = 1000000000000; //컴파일 에러
+      long var4 = 1000000000000L; //int 타입의 저장 범위를 넘어 l이나 L을 붙여서 초기화
+
+        System.out.println(var1);
+        System.out.println(var2);
+        System.out.println(var4);
+
+
+
+    }
+
+}
