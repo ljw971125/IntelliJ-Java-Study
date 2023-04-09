a50a1d5d26c6d9381e18391dd9a33b1ca473ca41
ljw971125 <ljw971125@naver.com>
2023-04-09 18:12:50 +0900

    자바 20230409 논리타입 boolean

---
+++ Java_Study/Java_20230409_Study/src/Study_02.java
@@ -0,0 +1,10 @@
+public class Study_02 {
+    public static void main(String[] args) {
+        boolean stop = false; //값이 true면 중지합니다 값이 false면 시작합니다를 출력
+        if(stop){ //만약 stop의 값이 true 이면
+            System.out.println("중지합니다.");
+        }else{ //stop의 값이 다른 값이면
+            System.out.println("시작합니다.");
+        }
+    }
+}
