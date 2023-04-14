e73b9cb0285e31d6bef2b2d985f0d18400c180b1
ljw971125 <ljw971125@naver.com>
2023-04-14 23:16:59 +0900

    자바 20230414 논리 부정 연산자 !

---
+++ Java_Study/Java_20230414_Study/src/Study_01.java
@@ -0,0 +1,12 @@
+public class Study_01 {
+    public static void main(String[] args) {
+        boolean play = true;
+        System.out.println(play);
+
+        play = !play; //true를 false로
+        System.out.println(play);
+
+        play = !play; //false를 true로 바꿈
+        System.out.println(play);
+    }
+}
