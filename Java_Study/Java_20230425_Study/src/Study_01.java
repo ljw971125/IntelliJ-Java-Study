735a2269c17e0507b464eade813b3eb6db3d2ff4
ljw971125 <ljw971125@naver.com>
2023-04-25 23:58:49 +0900

    자바 20230425 비트 이동 연산자 >>>

---
+++ Java_Study/Java_20230425_Study/src/Study_01.java
@@ -0,0 +1,7 @@
+public class Study_01 {
+    public static void main(String[] args) {
+        System.out.println("1 << 3 = " + (1 << 3)); // 8
+        System.out.println("-8 >> 3 = " + (-8 >> 3)); // -1
+        System.out.println("-8 >>> 3 = " + (-8 >>> 3)); // 536870911
+    }
+}
