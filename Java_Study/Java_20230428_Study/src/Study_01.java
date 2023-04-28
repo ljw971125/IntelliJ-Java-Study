19cd7d027c69fb089e7d9c3d051736cc56850a00
ljw971125 <ljw971125@naver.com>
2023-04-28 23:49:42 +0900

    자바 20230428 ++, --

---
+++ Java_Study/Java_20230428_Study/src/Study_01.java
@@ -0,0 +1,8 @@
+public class Study_01 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = (++x) + (y--);
+        System.out.println(z);
+    }
+}
