2db18425674013ee39ccd546bd330f352fb64afa
ljw971125 <ljw971125@naver.com>
2023-05-01 21:43:44 +0900

    자바 20230501 if문

---
+++ Java_Study/Java_20230501_Study/src/Study_01.java
@@ -0,0 +1,14 @@
+public class Study_01 {
+    public static void main(String[] args) {
+        int score = 93;
+
+        if(score>=90){
+            System.out.println("점수가 90보다 큽니다.");
+            System.out.println("등급은 A 입니다.");
+        }
+        if (score<90)
+            System.out.println("점수가 90보다 큽니다.");
+            System.out.println("등급은 B 입니다."); // if문과는 상관없는 실행문
+
+    }
+}
