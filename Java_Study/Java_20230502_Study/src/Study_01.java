9283b1180ee671da762c818ada7abeeadcc1a0e7
ljw971125 <ljw971125@naver.com>
2023-05-02 22:34:13 +0900

    자바 20230502 if, else

---
+++ Java_Study/Java_20230502_Study/src/Study_01.java
@@ -0,0 +1,14 @@
+public class Study_01 {
+    public static void main(String[] args) {
+        int score = 85;
+
+        if(score>=90){
+            System.out.println("점수가 90보다 큽니다.");
+            System.out.println(("등급은 A 입니다."));
+        }
+        else{ //Score < 90일 경우
+            System.out.println("점수가 90보다 작습니다.");
+            System.out.println("등급은 B 입니다.");
+        }
+    }
+}
