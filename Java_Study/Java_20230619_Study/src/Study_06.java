10717217d9f76a9248d3e7747f5318428165eb68
ljw971125 <ljw971125@naver.com>
2023-06-19 22:20:42 +0900

    자바 20230619 for반복문 중첩 for문을 사용해 방정식의 해 구하기

---
+++ Java_Study/Java_20230619_Study/src/Study_06.java
@@ -0,0 +1,12 @@
+public class Study_06 {
+    public static void main(String[] args) {
+        for(int a=1; a<=10; a++){
+            for(int b=1; b<=10; b++){
+                if((4*a)+(5*b)==60){
+                    System.out.println("("+a+","+b+")");
+                }
+
+            }
+        }
+    }
+}
