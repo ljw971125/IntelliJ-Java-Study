7b4f318b5ec5df4ac47de6d1e65b9b964cd537d0
ljw971125 <ljw971125@naver.com>
2023-09-05 16:41:13 +0900

    자바 20230905 인스턴스 멤버와 this

---
+++ Java_Study/Java_20230905_Study/src/Car1Example.java
@@ -0,0 +1,9 @@
+public class Car1Example {
+    public static void main(String[] args) {
+        Car1 myCar = new Car1("포르쉐");
+        Car1 yourCar = new Car1("벤츠");
+
+        myCar.run();
+        yourCar.run();
+    }
+}
